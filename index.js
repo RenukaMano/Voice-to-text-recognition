383ee970b5dd8d3ad9e352b238ac6e692963e55e
RenukaMano <renuka.mklb@gmail.com>
2021-03-20 14:59:07 +1100

    Created a voice to text recognition app

---
+++ index.js
@@ -0,0 +1,12 @@
+let speak = document.getElementById('speak');
+    let textarea = document.getElementById('textarea');
+    let SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
+    let recognition = new SpeechRecognition();
+    speak.addEventListener('click', function () {
+        recognition.start();
+        textarea.innerHTML = '...speaking';
+    })
+    recognition.onresult = function (e) {
+        let transcript = e.results[0][0].transcript;
+        textarea.innerHTML = transcript;
+    }
